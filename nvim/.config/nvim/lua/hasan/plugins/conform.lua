06da647de69ac332a3eef01041cf2fe6d2632d5e
M.Hasan Yumuşak <hasan.rualabs@gmail.com>
2026-08-29 23:38:08 +0300

    Add conform.nvim with manual formatting keymap

---
+++ nvim/.config/nvim/lua/hasan/plugins/conform.lua
@@ -0,0 +1,48 @@
+-- Formatting: explicit, never on save
+return {
+	"stevearc/conform.nvim",
+	dependencies = {
+		{
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			dependencies = { "mason-org/mason.nvim" },
+			opts = {
+				ensure_installed = {
+					"stylua", -- Lua
+					"prettier", -- JS, TS, Svelte, HTML, CSS, JSON, YAML, Markdown
+					"clang-format", -- C and C++
+				},
+			},
+		},
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = { "n", "v" },
+			desc = "Format buffer or selection",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- ruff is already installed as an LSP server; its formatter is the same binary
+			python = { "ruff_format" },
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			svelte = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+		},
+		-- No format_on_save: formatting is always triggered explicitly
+	},
+}
