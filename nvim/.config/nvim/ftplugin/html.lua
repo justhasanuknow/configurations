ae5f8798c784c88cfaec53a600702e170772f31b
M.Hasan Yumuşak <hasan.rualabs@gmail.com>
2026-08-30 00:08:51 +0300

    Add ftplugin settings for web, lua and markdown

---
+++ nvim/.config/nvim/ftplugin/html.lua
@@ -0,0 +1,4 @@
+-- Web ecosystem convention: two spaces
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
---
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- Prose reads better wrapped
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.spell = false
